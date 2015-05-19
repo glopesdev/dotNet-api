5e13bf0891e249c7933cf69902a1117f190ed6e9
glopesdev <goncaloclopes@gmail.com>
2015-05-19 17:28:44 +0100

    Added AssemblyInfo.cpp with assembly metadata and a shared title for both platform architectures.

---
+++ AssemblyInfo.cpp
@@ -0,0 +1,36 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+//
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly:AssemblyTitleAttribute("BITalino")];
+[assembly:AssemblyDescriptionAttribute("")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("Filipe Silva")];
+[assembly:AssemblyProductAttribute("BITalino")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) PLUX - Wireless Biosignals, S.A. 2014-2015")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the value or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly:ComVisible(false)];
+[assembly:CLSCompliantAttribute(true)];
+[assembly:AssemblyVersionAttribute("1.1.0.0")];
+[assembly:AssemblyInformationalVersionAttribute("1.1.1")];
+[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
